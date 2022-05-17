43518c8885879c38cc9baae1693e91f045e4757d
Nisal Bandara <nisal.bandara@outlook.com>
2022-05-18 02:00:15 +0530

    Add test for `vector_test()`

--- test/vector_test.cc
+++ test/vector_test.cc
@@ -18,3 +18,12 @@ TEST(VectorTest, VectorIsFreed)
 	vector_free(&v, NULL);
 	EXPECT_EQ(v, nullptr);
 }
+
+TEST(VectorTest, ShouldReturnSize)
+{
+	struct vector *v = vector_new(10, sizeof(int));
+	EXPECT_NE(v, nullptr);
+	size_t size = vector_size(v);
+	EXPECT_EQ(size, 0);
+	vector_free(&v, NULL);
+}
